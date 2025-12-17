22ee0b5cf0152eb01b6b851b5c4119e62d817932
shemankubana <shemaivan27@gmail.com>
2025-12-17 14:23:33 +0200

    data import

---
+++ Regression Working File.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv('https://dev-cms.us-east-1.linodeobjects.com/student_mat_2173a47420.csv', sep=";")
+
